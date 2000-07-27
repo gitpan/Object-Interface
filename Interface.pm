7c2841c9cd75490e99fc53238a9f41a51066824a
Gerad Welch <welch.119@osu.edu>
2000-07-27 05:00:47 +0000

    Import of GWELCH/Object-Interface-1.1 from CPAN.
    gitpan-cpan-distribution: Object-Interface
    gitpan-cpan-version:      1.1
    gitpan-cpan-path:         GWELCH/Object-Interface-1.1.zip
    gitpan-cpan-author:       GWELCH
    gitpan-cpan-maturity:     released

--- Interface.pm
+++ Interface.pm
@@ -8,20 +8,29 @@
 # ::Interface    adpn  Specificy pure virtual base classes ala C++  GWELCH
 #
 #
-# Author:    Gerad Welch <gwelch@computer.org>
-#                        <welch.119@osu.edu> for development issues
+# Author:	Gerad Welch <gwelch@computer.org>
+#			    <welch.119@osu.edu> for development issues
+#
+# Contributors:
+#   [STH] Shay Harding <sharding@geocities.com>
 #
 # Copyright:
-#   Copyright (c) 1999 Gerad Welch.  All rights reserved.
+#   Copyright (c) 1999-2000 Gerad Welch.  All rights reserved.
 #   This program may be freely redistributed, though all useful
 #   modifications must be sent back to the author for examination and
 #   and possible incorporation into future versions. =)
 #
 # Revision Log:
 #
-# 1999.09.02 GMW  Finished preliminary version.
+# 2000.07.16 GMW  A version that really works....  Unfortunately, this
+#		  only runs under versions of Perl which support CHECK
+#                 and the warning pragma (I think only 5.6 or better).
+# 2000.05.11 STH  Added output of all missing methods at program
+#		  termination, as well as some other miscellaneous useful
+#		  stuff.
 # 1999.09.18 GMW  Removed 'require' interface for simplicity.  Cleaned up
-#                 things a /little/ bit....
+#		  things a /little/ bit....
+# 1999.09.02 GMW  Finished preliminary version.
 #
 # Note:
 # Someday it might be nice to have an 'object' pragma, so one could say
@@ -32,9 +41,17 @@ package Object::Interface;
 
 use strict;
 
-use vars qw( $VERSION );
+use vars qw( $VERSION $DEBUG $list );
 
-$VERSION = '1.0';
+$VERSION = '1.1';
+$DEBUG = 0;
+
+BEGIN
+{
+
+  $list = [];
+
+}
 
 sub import
 {
@@ -43,44 +60,100 @@ sub import
   shift;
 
   my @required = @_;
-#  print STDERR "Interface methods:\n@required\n";
 
-  # Find out what module called us
+  # Find out what module called us (the last require'd or use'd module)
   my @caller;
   my $i = 0;
+
   while ( 1  ) {
     @caller = caller $i++;
     die "Unable to find derived class!\n" if ! @caller;
-    last if defined $caller[7];
+    # This must be compared explicitly to 1, as sometimes caller() returns
+    # non-null values that aren't 1.  Think that's a bug.  Seems that this
+    # changed between Perl 5.00x and 5.6.
+    if ( defined $caller[7] ) {
+      last if "$caller[7]" eq "1";
+    }
   }
-#  print STDERR "caller() info:\n@caller\n";
 
-  # Extract all sub names from the calling module
-  my %syms;
-  my $is_sub;
-  eval '%syms = %' . $caller[0] . '::';
-  my @present = map {
-                      eval '$is_sub = defined ' . $syms{$_} . '{CODE}';
-                      $is_sub ? ( $_ ) : ( );
-                    } keys %syms;
-#  print STDERR "Derived class's methods:\n@present\n";
+print STDERR <<"EOS" if $DEBUG;
 
-  # Check to see what's there and what's not
-  my $sym;
-  my $flag;
-  while ( 1 ) {
-    $sym = shift @required;
-    last if ! defined $sym;
-    @present = map { ( $sym eq $_ ) ? ( $flag = $sym, () )[1] : ( $_ ) } @present;
-    die "Pure virtual function '$sym' not defined in $caller[1].\n"
-      if ! defined $flag;
-    undef $flag;
-  }
+use Object::Interface called from $caller[0]
+( @caller )
+Interface methods: @required
+
+EOS
+
+  # Save the frame information for later checking
+  push @$list, [ \@caller, \@required ];
 
 }
 
-# Written to Shiina Ringo's "Koko de Kissu-shite" playing on really bad
-# speakers.  Yo to Brian Lemieux and Ed Wahl.  KDM, go to school, dammit!
+CHECK
+{
+
+  my @caller;
+  my @required;
+
+  my @syms;
+
+  my @present;
+  my $is_sub;
+
+  my ( $sym, $flag );
+  my @errors;
+
+  my $error = 0;
+
+  foreach my $frame ( @$list ) {
+
+    @caller = @{$frame->[0]};
+    @required = @{$frame->[1]};
+
+    # Extract all sub names from the calling module
+    eval '@syms = keys %' . $caller[0] . '::';
+
+do {
+  map { print STDERR "$_ " } @syms;
+  print "\n";
+} if $DEBUG;
+
+    # Check to see if the symbol's defined in the package's symbol table
+
+    no warnings;
+
+    @present = map {
+                     eval "\$is_sub = defined \$${caller[0]}::{$_}";
+                     $is_sub ? ( $_ ) : ( );
+                   } @syms;
+
+    use warnings;
+
+print STDERR "Derived class's methods: @present\n\n" if $DEBUG;
+
+    # Check to see what's there and what's not
+
+    @errors = ();
+    foreach my $sym ( @required ) {
+      @present = map { ( $sym eq $_ ) ? ( $flag = $sym, () )[1] : ( $_ ) } @present;
+      push @errors, $sym if ! defined $flag;
+      undef $flag;
+    }
+
+    if ( @errors ) {
+      print STDERR sprintf "Pure virtual function%s < " . join( ', ', @errors ) . " > not defined in $caller[1].\n",
+        ( @errors == 1 ? '' : 's' );
+      $error = 1;
+    }
+
+  }
+
+  die "Execution aborted.\n" if $error;
+
+}
+
+# Codesong: Plastic Tree, "Trance Orange"
+# Yo to Yohei, Tomi, and Yasu -- party animals of my own nature.
 
 1;
 
